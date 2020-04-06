ae8ec32509aba499e16102c05a41cb6c0b376bc0
Pitt Wu <kgb00128@gmail.com>
2020-04-06 13:59:57 +0800

    [Fixed] 調整錯誤頁面

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
-  .error__wrap this error page
+  .error-wrap
+    .error-text 找不到這個頁面
+    nuxt-link.btn(to="/") 回到首頁
 </template>
 
 <script>
@@ -11,5 +13,28 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+  @import '@/assets/scss/share.scss';
 
+  .error-wrap {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+    font-size: to-rem(40px);
+    margin-top: to-rem(64px);
+  }
+
+  .btn {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: to-rem(200px);
+    height: to-rem(60px);
+    font-size: to-rem(28px);
+    color: $light;
+    margin-top: to-rem(24px);
+    border: 1px solid $border-line;
+    background: $header-bg;
+    border-radius: to-rem(10px);
+  }
 </style>
